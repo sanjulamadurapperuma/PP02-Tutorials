7fa10164ac521e89a6a2e89881c3a2b1d56ca273
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-02-28 12:07:14 +0530

    PP02 - Tutorial - This is the correct answer to Tutorial 2

---
+++ src/com/iit/sanjula/tutorial3/Question4/Appointment.java
@@ -0,0 +1,30 @@
+package com.iit.sanjula.tutorial3.Question4;
+
+public abstract class Appointment {
+    public String description;
+    public int day;
+    public int month;
+    public int year;
+
+    public Appointment(String description, int day) {
+        this.description = description;
+        this.day = day;
+    }//Daily
+
+    public Appointment(String description, int day, int month) {
+        this.description = description;
+        this.day = day;
+        this.month = month;
+    }//Monthly
+
+    public Appointment(String description, int day, int month, int year) {
+        this.description = description;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }//Onetime
+
+    abstract boolean occursOn(int year, int month, int day);
+
+    abstract String getDescription();
+}
---
+++ src/com/iit/sanjula/tutorial3/Question4/Monthly.java
@@ -0,0 +1,17 @@
+package com.iit.sanjula.tutorial3.Question4;
+
+public class Monthly extends Appointment{
+    public Monthly(String description, int day, int month) {
+        super(description, day, month);
+    }
+
+    @Override
+    boolean occursOn(int year, int month, int day) {
+        return (this.day == day) && (this.month == month);
+    }
+
+    @Override
+    String getDescription() {
+        return null;
+    }
+}
