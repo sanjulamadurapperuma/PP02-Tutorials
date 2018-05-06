5e74e642296f4b202294674a9e4ea7bf500bc42d
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-05-06 17:03:08 +0530

    Added some practice examples from armstrong java questions

---
+++ src/com/iit/sanjula/Practice/Test.java
@@ -0,0 +1,21 @@
+package com.iit.sanjula.Practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        list.add("red");
+        list.add("red");
+        list.add("green");
+
+        String element = "red";
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).equals(element)){
+                list.remove(element);
+            }
+        }
+
+    }
+}
---
+++ src/lectures/Lecture10/Tester.java
@@ -0,0 +1,7 @@
+package lectures.Lecture10;
+
+public class Tester {
+    public static void main(String[] args) {
+
+    }
+}
